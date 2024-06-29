24cfecbe11e67ef2bfba5ecfcc85a0774f8d3682
Manoj Kumar Reddy P <thejonamreddy@gmail.com>
2024-06-29 15:14:53 +0530

    Updated js

--- 238-product-of-array-except-self/brute-force-2.js
+++ 238-product-of-array-except-self/brute-force-2.js
@@ -8,6 +8,7 @@ var productExceptSelf = function (nums) {
   let product = 1
   let zeroCount = 0
 
+  /* Calculate the product of all the elements and keep a track of number of zeroes */
   for (let i = 0; i < nums.length; i++) {
     if (nums[i] === 0) {
       zeroCount++
@@ -16,11 +17,16 @@ var productExceptSelf = function (nums) {
     }
   }
 
+  /* If there are two or more zeros in the array, the output will always be zero for every element */
   if (zeroCount > 1) {
     return res.fill(0)
   }
 
   for (let i = 0; i < nums.length; i++) {
+    /* 
+      If there is exactly one zero in the array, the output for the position of that zero will be the product of all non-zero elements in the array. 
+      For all other positions, the output will be zero since their products will involve the zero 
+    */
     if (zeroCount === 1) {
       if (nums[i] === 0) {
         res[i] = product
